1c81f2b7fb9107a16dc7c7f518ed554e9a722d72
Carlos Martinez <itscarlosisaac@gmail.com>
2018-07-05 21:16:38 -0400

    Created footer component

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <footer>
+      <footer className="app__footer">
         <p>
         © 2018 <a href="http://www.itscarlosisaac.com" target="_blank">Carlos I. Martinez</a>. All rights reserved.
          - Powered by <a href="https://github.com/Giphy/GiphyAPI" target="_blank">Giphy</a>
