3c8a89dc84835a7acbde90202141a5f511e0e2d6
Carlos Martinez <itscarlosisaac@gmail.com>
2018-07-05 21:16:45 -0400

    Created header component

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
-import GifServices from '../services/gifServices';
+import Canvas from './Canvas';
+import ImageCard from './ImageCard';
+import SearchFor from './SearchFor';
+
+import Logo from './Logo';
 
 export default class Header extends Component {
-
-  componentWillMount = () => {
-    console.log(GifServices.FecthData('ryan', 10, 0))
-  }
-  
   render() {
     return (
-      <header>
-        <h1>Header</h1>
+      <header className="app__header">
+        {/* <Canvas /> */}
+        <Logo w="140" />
+        <h2 className="app__header--title">Welcome to the funniest gif search website.</h2>
+        <SearchFor />
+        {
+          /* 
+            this.state.arr.map( (i) => {
+              return <ImageCard key={i.id} title={i.title} image={i.images.downsized.url} />
+            }) 
+          */
+        }
       </header>
     )
   }
-}
+} 
