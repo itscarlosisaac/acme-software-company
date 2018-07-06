893b40b43c45bc9b46b4491585830e08a3caf14b
Carlos Martinez <itscarlosisaac@gmail.com>
2018-07-05 21:17:06 -0400

    Added gif services

---
+++ client/services/gifServices.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+const API_KEY = "LM5cFIkxw5OOGvOsk1fBYKkXjoxyQ5g2"
+
+export default {
+  FecthData(term, limit, offset, cb){
+    const URL = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=${API_KEY}&limit=${limit}&offset=${offset}`
+    axios.get(URL)
+      .then(response => {
+        const data = response.data.data
+        return data;
+      })
+      .then((data) => cb(data) )
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+}
