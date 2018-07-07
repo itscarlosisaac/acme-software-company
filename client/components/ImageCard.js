f717deb2877dac95c94ea51a1285eabc82ed027a
Carlos Martinez <itscarlosisaac@gmail.com>
2018-07-07 17:08:33 -0400

    Added animation intro to thumbnails.

--- client/components/ImageCard.js
+++ client/components/ImageCard.js
@@ -2,22 +2,12 @@ import React, { Component } from 'react'
 
 export default class ImageCard extends Component {
 
-  constructor(props){
-    super(props);
-    this.cardRef = React.createRef();
-  }
-
   handleClick = () => {
     this.props.handleThumbClick(this.props.id) 
   }
-
-  componentDidMount = () => {
-    this.cardRef.current.classList.add('image__card--animated')
-  }
-
   render() {
     return (
-      <div ref={this.cardRef} className="image__card" onClick={this.handleClick}>
+      <div className="image__card" onClick={this.handleClick}>
         <div className="image__card--thumb">
           <img src={this.props.image} alt={this.props.title} />
         </div>
