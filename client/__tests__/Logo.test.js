bbb1670849ab6797856969b2cf566457598203e2
Carlos Martinez <itscarlosisaac@gmail.com>
2018-12-03 01:12:05 -0400

    Tests - Logo Component.

---
+++ client/__tests__/Logo.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Logo from '../components/Logo';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from  'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+configure({adapter: new Adapter() });
+
+describe(`<Logo /> - Logo Component`, () => {
+  it(`Should accept a size prop`, () => {
+    const wrapper = shallow(<Logo w={140} />);
+    expect(wrapper.instance().props.w).toBe(140);
+  });
+
+  it(`Should render an image tag`, () => {
+    const wrapper = shallow(<Logo w={140} />);
+    expect(wrapper.find('img').type()).toEqual('img')
+  });
+
+  it(`Should Match the snapshot`, () => {
+    const tree = shallow(<Logo w={140} />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
+});
